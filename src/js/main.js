f92698de75b5a09fbf02773ec6e8acc1c92e27f0
Alexandre BM <s@rednaks.tn>
2012-08-24 18:37:03 +0100

    game over plays number and X/O

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 var boardIds = {a0: null,a1: null,a2: null,b0: null,b1: null,b2: null,c0: null,c1: null,c2: null};
 var player = Math.floor(Math.random()*10)%2; // random value: 1 or 0
-
+var plays = 0;
 window.addEventListener('click',MainMorpion, false);
 
 function MainMorpion(event){
@@ -9,8 +9,12 @@ function MainMorpion(event){
 		console.log('DEBUG: The clicked id\'s element : '+id);
 		if(player%2) { // player 1
 			alert('Player 1');
-		}else
+			document.getElementById(id).innerHTML = 'O'
+		}else{
 			alert('Player 2');
+			document.getElementById(id).innerHTML = 'X'
+		}
+		plays++;
 		gameOver();
 		player++;
 	}else
@@ -19,6 +23,8 @@ function MainMorpion(event){
 
 function gameOver() {
 
-	if(false)
+	if(plays==9){
+		alert('GameOver !');
 		alert('Player : '+((player%2)+1)+' is the winner !');
+	}
 }

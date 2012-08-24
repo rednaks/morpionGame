4646ac9587815f4d2c9a8a0064ae9c34e5d92059
Alexandre BM <s@rednaks.tn>
2012-08-24 17:52:01 +0100

    managing players

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
 var boardIds = {a0: null,a1: null,a2: null,b0: null,b1: null,b2: null,c0: null,c1: null,c2: null};
+var player = Math.floor(Math.random()*10)%2; // random value: 1 or 0
+
 window.addEventListener('click',MainMorpion, false);
 
 function MainMorpion(event){
 	var id = event.originalTarget.id;
 	if(id in boardIds){
 		console.log('DEBUG: The clicked id\'s element : '+id);
+		if(player%2) { // player 1
+			alert('Player 1');
+		}else
+			alert('Player 2');
+		player++;
 	}else
 		console.log('DEBUG: Nothing todo');
 }
